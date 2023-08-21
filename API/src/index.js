0933a3248fbfb3dc11c7b13a4dd8267a3601a026
DaniLP-200 <alejalopezpedroza@gmail.com>
2023-08-21 10:24:49 -0500

    actualizacion

--- API/src/index.js
+++ API/src/index.js
@@ -4,13 +4,18 @@ import express from 'express';
 import usuariosRoutes from './routes/usuarios.routes.js'
 import inventarioRoutes from './routes/inventario.routes.js'
 import {PORT} from './config.js' 
+import cors from 'cors'
 const app =express();
 
+
+//const WhiteList= ['http://localhost:4200']
 app.use(express.json());
+app.use(cors());
 
 //rutas 
 app.use('/api',usuariosRoutes);
 app.use ('/api', inventarioRoutes)
+app.use("/", express.static("html"));
 
 //ruta no encontrada
 
